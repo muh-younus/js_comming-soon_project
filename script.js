a5dc5c0a4599e5218c52201daea80d1ab78b2720
muh-younus <braq369@gmail.com>
2024-12-24 00:27:08 +0500

    another commit

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-var countDownDate = new Date('Dec 25, 2024 00:00:00').getTime();
-var x = setInterval(function(){
+var countDownDate = new Date('Dec 29, 2024 00:00:00').getTime();
+ setInterval(function(){
 
     var now = new Date().getTime();
     var distance = countDownDate - now;
     // var difference = new Date(distance);
     // console.log("The difference is: ", difference.toString());
 
-    var days = Math.floor(distance/ (1000 * 60 * 60 * 24));
+    var days = Math.floor(distance/(1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance%(1000 * 60 * 60 * 24)/(1000 * 60 * 60));
+    var minutes = Math.floor(distance%(1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)/1000));
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+   
     
     
-} )
+} ,1000)
+
+
 
 
 // var now = new Date().getTime();
